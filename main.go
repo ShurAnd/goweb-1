6d371d992aef63edadb31d2764addb48b818ade2
andrey shurmanov <shurmanovand@gmail.com>
2025-07-24 18:44:40 +0300

    some fix

--- main.go
+++ main.go
@@ -5,18 +5,5 @@ import "github.com/gin-gonic/gin"
 func main() {
 	router := gin.Default()
 
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "hello world",
-		})
-	})
-
-	router.GET("/hello/:name", func(c *gin.Context) {
-		name := c.Param("name")
-		c.JSON(200, gin.H{
-			"message": "Hello " + name + "!",
-		})
-	})
-
 	router.Run()
 }
